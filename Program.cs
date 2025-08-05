7d1f5ad5e05ce328e1d8dd0e1cd76eacd0381d70
DanielTrujilloTapia <22300119@uttt.edu.mx>
2025-08-04 22:54:04 -0600

    Se agrego el ClockSkew = TimeSpan.Zero en program para que lea el tiempo de expiracion mas exactamente

--- Program.cs
+++ Program.cs
@@ -25,7 +25,9 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             ValidateIssuerSigningKey = true,
             ValidIssuer = jwtSettings["Issuer"],
             ValidAudience = jwtSettings["Audience"],
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["SecretKey"]))
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["SecretKey"])),
+            ClockSkew = TimeSpan.Zero
+
         };
 
         options.Events = new JwtBearerEvents
